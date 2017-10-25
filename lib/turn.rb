873efb114c4a741eab97ed87423a11de7bd2dd8c
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-25 05:45:54 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,2 +1,24 @@
 def turn
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+end
+
+def input_to_index(input)
+  (input.to_i) - 1
+def display_board(board)
+end
+
+def valid_move?(board, index)
+end
+
+def position_taken?(board, index)
+  if(board[index] == " " || board[index] == "" || board[index] == nil)
+    false
+  else
+    true
+  end
+end
+
+def move(board, index, player = "X")
 end
