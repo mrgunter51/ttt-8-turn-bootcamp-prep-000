c289248564fe7bbff02bd1d512202f9325043253
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-25 05:48:36 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,6 +9,11 @@ def input_to_index(input)
 end
 
 def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def valid_move?(board, index)
